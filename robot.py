a4a3576fa9f57f8a5328e63086826af09135af80
Joe <73498493+jserra99@users.noreply.github.com>
2021-11-15 15:59:38 -0800

    Create robot.py

---
+++ robot.py
@@ -0,0 +1,12 @@
+'''
+GOALS:
+    - Have an easily customizable autonomous mode that can take vector inputs and do those movements
+        -- python GUI app that outputs vector data and autonomous "map" into a csv
+        -- autonomous reads a csv
+        -- Figure out how to actually go an exact distance without guessing & checking times
+    - Working swerve drive
+    - Configuration file (preferabbly json) for most if not all robot settings
+    - Communicate with vision using networktables
+    - Get motor framework down (assume we are using talonfx controllers)
+
+'''
