e851685ffe4f31fe54711fc447d09f66dad7c366
Joe <73498493+jserra99@users.noreply.github.com>
2021-12-20 17:08:42 -0800

    Add files via upload

--- robot.py
+++ robot.py
@@ -1,103 +1,175 @@
-from ctre._ctre import AbsoluteSensorRange, SensorInitializationStrategy
+'''
+GOALS:
+    - Have an easily customizable autonomous mode that can take coordinate inputs and do those movements
+        -- python GUI app that outputs vector data and autonomous "map" into a csv
+        -- autonomous reads a csv
+        -- Figure out how to actually go an exact distance without guessing & checking times
+            --- Can be done using circumference of wheels
+    - Working swerve drive
+    - Configuration file (preferabbly json) for most if not all robot settings
+    - Communicate with vision using networktables
+    - Get motor framework down (assume we are using talonfx controllers)
+    -- Nearly all completed
+TODO:
+- Not really anything as far as a bare bones only falcon drivetrain robot goes this is everything.
+- Still need to integrate vision but that is hard when you do not even know what your robot will be doing
+- Still need to integrate auxiliary files but yet again without any auxiliary systems as of yet, this is impossible
+- Need to implement different autonomous modes that can have a default and be toggleable we probably don't want to default to dumb autonomous
+
+NOTE - Current Features:
+- Autonomous including pathing and functions based on recorded session
+- Easy drivetrain zeroing through smartdashboard
+- Responsive drivetrain with PID control and functions such as coast and stationary
+- easily expandible auxiliary and drivetrain functions
+- easier to read and edit settings through the external config.json file
+
+How to use:
+- Teleoperated Mode:
+  - Pretty self-explanatory
+  
+- Autonomous Mode:
+  - Runs the pre-selected autonomous mode set in smartdashboard, the default is called "default"
+
+- Test Mode:
+  - Records an autonomous path and exports it --ON THE ROBORIO-- using datetime and is located in paths, would recommend using winscp
+    to retrieve json
+
+Installing Dependencies:
+py -3.99 -m pip install robotpy robotpy-ctre robotpy-navx
+'''
 import wpilib
-import ctre
-from wpilib._wpilib import Joystick
-import wpilib.controller
-import math
+import json
+import os
+import driveTrain
+import driverStation
+from time import strftime, gmtime
+from networktables import NetworkTables
+import navx
+import threading
+
+cond = threading.Condition()
+notified = False
+def connectionListener(connected, info):
+	print(info, '; Connected=%s' % connected)
+	with cond:
+		notified = True 
+		cond.notify()
+
+NetworkTables.initialize()
+NetworkTables.addConnectionListener(connectionListener, immediateNotify=True) # this is also broken
+vision = NetworkTables.getTable('aetherVision')
 
 class MyRobot(wpilib.TimedRobot):
+
     def robotInit(self):
-        turnOffset = -20
-        self.turnMotor = ctre.TalonFX(1)
-        self.driveMotor = ctre.TalonFX(3)
-        self.absoluteEncoder = ctre.CANCoder(2)
-        self.absoluteEncoder.configMagnetOffset(turnOffset)
-        #self.driveMotor = ctre.TalonFX(2)
-        self.absoluteEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition)
-        self.absoluteEncoder.configAbsoluteSensorRange(AbsoluteSensorRange.Signed_PlusMinus180)
-        self.turnMotor.configIntegratedSensorAbsoluteRange(AbsoluteSensorRange.Unsigned_0_to_360)
-        #self.turnMotor.config
-        self.turnController = wpilib.controller.PIDController(0.005, 0.0025, 0)
-        self.turnController.enableContinuousInput(-180, 180)
-        self.previousTarget = 0
-        self.initiateModule()
-        # self.turnMotor.configIntegratedSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition)
+        '''
+        This function is called upon program startup and
+        should be used for any initialization code.
+        '''
+        with open(f"{os.getcwd()}./config.json", "r") as f1: #incorrect method of loading json same as with in the drivetrain file.
+            self.config = json.load(f1)
+        self.driveTrain = driveTrain.driveTrain(self.config)
+        self.driverStation = driverStation.driverStation(self.config)
+        self.navx = navx.AHRS.create_spi()
+        self.navx.reset()
+        self.Timer = wpilib.Timer()
+
+    def autonomousInit(self):
+        '''This function is run once each time the robot enters autonomous mode.'''
+        autoPlanName = wpilib.SmartDashboard.getString("Auto Plan", "default")
+        with open(f"{os.getcwd()}./paths/{autoPlanName}.json", 'r') as plan:  
+            self.autoPlan = json.dump(plan)
+        self.autonomousIteration = 0
+        self.navx.reset()
+        self.Timer.reset()
         
-    def initiateModule(self):
-        self.turnMotor.setSelectedSensorPosition(self.absoluteEncoder.getAbsolutePosition() *2048*12.8/360)
-        
-    def zeroModule(self):
-        pass
     
-    def move(self):
-        pass
-    
-    def teleopInit(self) -> None:
-        self.joystick = wpilib.Joystick(0)
-        self.turnMotor.setSelectedSensorPosition(self.absoluteEncoder.getAbsolutePosition() *2048*12.8/360)
-    
-    def teleopPeriodic(self) -> None:
-        '''self.targetAngle = wpilib.SmartDashboard.getNumber("target:", 180)
-        self.turnOnly(self.targetAngle)'''
-        self.diagnosticPeriodic()
-        x = self.joystick.getX()
-        y = self.joystick.getY()
-        wpilib.SmartDashboard.putNumber("X:", x)
-        wpilib.SmartDashboard.putNumber("Y:", y)
-        if (x > 0.1 or x < -0.1) or (y > 0.1 or y < -0.1):
-            angle = math.degrees(math.atan2(x, y))
-            if angle > 180:
-                angle -= 180
-            self.previousTarget = angle
-            wpilib.SmartDashboard.putNumber("angle:", angle)
-            self.turnOnly(angle)
-            speed = math.hypot(x, y) / 4
-            wpilib.SmartDashboard.putNumber("speed:", speed)
-            self.driveMotor.set(ctre.ControlMode.PercentOutput, speed)
+    def autonomousPeriodic(self):
+        '''This function is called periodically during autonomous.'''
+        # deadzones are already filtered so no reason to do any of that here
+        if (self.autonomousIteration < len(self.autoPlan)): # to prevent any index out of range errors
+            switches = self.autoPlan[self.autonomousIteration]
+        if self.Timer.get() > self.config["matchSettings"]["autonomousTime"]:
+            # auto is over
+            self.Timer.stop()
+            self.stopAll()
         else:
-            self.turnOnly(self.previousTarget)
-            self.driveMotor.set(ctre.ControlMode.PercentOutput, 0)
-            wpilib.SmartDashboard.putNumber("speed:", 0)
-            
-            
-    def diagnosticPeriodic(self):
-        ''' Purpose is to provide diagnostics on the robot no matter the mode. '''
-        motorPosition = self.turnMotor.getSelectedSensorPosition(0)
-        motorPosition = ((motorPosition % (2048*12.8)) * 360/(2048*12.8))
-        if motorPosition > 180:
-            motorPosition -= 360
-        absolutePosition = self.absoluteEncoder.getAbsolutePosition()
-        angleTolerance = 5
-        # checking tolerances
-        if motorPosition + angleTolerance > absolutePosition and motorPosition - angleTolerance < absolutePosition:
-            wpilib.SmartDashboard.putBoolean("within range", True)
-        else:
-            wpilib.SmartDashboard.putBoolean("within range", False)
-            
-    def disabledPeriodic(self) -> None:
-        motorPosition = self.turnMotor.getSelectedSensorPosition(0)
-        motorPosition = ((motorPosition % (2048*12.8)) * 360/(2048*12.8))
-        if motorPosition > 180:
-            motorPosition -= 360
-        wpilib.SmartDashboard.putNumber("internalPosition:", motorPosition)
-        wpilib.SmartDashboard.putNumber("absolutePosition:", self.absoluteEncoder.getAbsolutePosition())
-        wpilib.SmartDashboard.putNumber("RAW:", self.turnMotor.getSelectedSensorPosition(0))
-        self.diagnosticPeriodic()
-    
-    def turnOnly(self, angle):
-        motorPosition = self.turnMotor.getSelectedSensorPosition(0)
-        motorPosition = ((motorPosition % (2048*12.8)) * 360/(2048*12.8))
-        if motorPosition > 180:
-            motorPosition -= 360
-        wpilib.SmartDashboard.putNumber("internalPosition:", motorPosition)
-        wpilib.SmartDashboard.putNumber("absolutePosition:", self.absoluteEncoder.getAbsolutePosition())
-        self.turnController.setSetpoint(angle)
-        turnSpeed = self.turnController.calculate(motorPosition)
-        '''turnDeadBand = 0.01
-        if turnSpeed < turnDeadBand:
-            turnSpeed = 0'''
-        self.turnMotor.set(ctre.ControlMode.PercentOutput, turnSpeed)
+            self.switchActions(switches)
+        self.autonomousIteration += 1
+        self.driveTrain.refreshValues()
+
+    def teleopInit(self):
+        self.navx.reset()
+        self.driveTrain.refreshValues()
         
+    def teleopPeriodic(self):
+        '''This function is called periodically during operator control.'''
+        switches = self.driverStation.checkSwitches()
+        switches["driverX"], switches["driverY"], switches["driverZ"] = self.evaluateDeadzones(switches["driverX"], switches["driverY"], switches["driverZ"])
+        self.switchActions(switches)
+        
+    def disabledPeriodic(self):
+        ''' Intended to update shuffleboard with drivetrain values used for zeroing '''
+        self.driveTrain.refreshValues()
+    
+    def testInit(self):
+        self.navx.reset()
+        self.Timer.reset()
+        self.autonomousSwitchList = []
+        self.hasMoved = False
+        wpilib.SmartDashboard.putBoolean("Recording", True)
+    
+    def testPeriodic(self):
+        ''' Intended to record a path for autonomous '''
+        switches = self.driverStation.checkSwitches()
+        switches["driverX"], switches["driverY"], switches["driverZ"] = self.evaluateDeadzones(switches["driverX"], switches["driverY"], switches["driverZ"])
+        if not self.hasMoved:
+            if switches["driverX"] != 0 or switches["driverY"] != 0 or switches["driverZ"] != 0:
+                self.hasMoved = True
+                self.Timer.start()
+        if self.hasMoved:
+            if self.Timer.get() > self.config["matchSettings"]["autonomousTime"]:
+                # autonomous recording period has ended
+                self.recording = False
+                self.Timer.stop()
+                self.stopAll()
+                wpilib.SmartDashboard.putBoolean("Recording", False)
+                dt_gmt = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
+                with open(f"{os.getcwd()}./paths/{dt_gmt}.json", 'w') as path:
+                    path.write(json.dump(self.autonomousSwitchList))
+            if self.recording:
+                switches["time"] = self.Timer.get()
+                self.autonomousSwitchList.append(switches) # adds 50x a second a little excessive maybe but needs to capture all inputs
+                self.switchActions(switches)
+        self.driveTrain.refreshValues()
+            
+    def switchActions(self, switchDict: dict):
+        ''' Actually acts on and calls commands based on inputs from multiple robot modes '''
+        if switchDict["driverX"] != 0 or switchDict["driverY"] != 0 or switchDict["driverZ"] != 0:
+            self.driveTrain.manualMove(switchDict["driverX"], switchDict["driverY"], switchDict["driverZ"], switchDict["navxAngle"])
+        else:
+            self.driveTrain.stationary()
+        if switchDict["swapFieldOrient"]:
+            self.driveTrain.fieldOrient = not self.driveTrain.fieldOrient # swaps field orient to its opposite value
+            wpilib.SmartDashboard.putBoolean("Field Orient", self.driveTrain.fieldOrient)
+        if switchDict["playEasterEgg"]:
+            self.driveTrain.easterEgg()
+        if switchDict["zeroDriveTrainEncoders"]:
+            self.driveTrain.zeroMotorEncoders()
+    
+    def evaluateDeadzones(self, x: float, y: float, z: float):
+        if not (x > self.config["driverStation"]["joystickDeadZones"]["xDeadZone"]):
+            x = 0
+        if not (y > self.config["driverStation"]["joystickDeadZones"]["yDeadZone"]):
+            y = 0
+        if not (z > self.config["driverStation"]["joystickDeadZones"]["zDeadZone"]):
+            z = 0
+        return x, y, z
+    
+    def stopAll(self):
+        ''' Exactly as it says, stops all of the functions of the robot '''
+        self.driveTrain.stationary()
+        # will add more as they come
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
