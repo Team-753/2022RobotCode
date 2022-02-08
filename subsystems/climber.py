6bcccf2dbb03c6eca8ec1914b5bc830a9a2c22bd
Benjamin Rawson <46731576+WizardOfTheBon@users.noreply.github.com>
2022-02-07 20:06:26 -0800

    I almost finished the Climber file

--- subsystems/climber.py
+++ subsystems/climber.py
@@ -5,10 +5,8 @@ import rev
 class Climber:
     def __init__(self, config = dict):
         self.config = config
-        self.leftShoulder = rev.CANSparkMax(self.config["Climber"]["leftShoulder_ID"], rev.MotorType.kBrushless)
-        self.leftShoulderEncoder = self.leftShoulder.getEncoder()
-        self.rightShoulder = rev.CANSparkMax(self.config["Climber"]["rightShoulder_ID"], rev.MotorType.kBrushless)
-        self.rightShoulderEncoder = self.rightShoulder.getEncoder()
+        self.leftShoulder = Shoulder(self.config["Climber"]["leftShoulder"]["ID"], self.config["Climber"]["leftShoulder"]["Name"])
+        self.rightShoulder = Shoulder(self.config["Climber"]["rightShoulder"]["ID"], self.config["Climber"]["rightShoulder"]["Name"])
         self.leftWinch = Winch(self.config["Climber"]["leftWinch"]["ID"], self.config["Climber"]["leftWinch"]["Name"])
         self.rightWinch = Winch(self.config["Climber"]["rightWinch"]["ID"], self.config["Climber"]["rightWinch"]["Name"])
         
@@ -35,12 +33,37 @@ class Shoulder:
     def __init__(self, sparkID, name):
         self.motor = rev.CANSparkMax(sparkID, rev.MotorType.kBrushless)
         self.currentLimit = 35 # This amperage limit has not been tested
-        self.motor.setSelectedSensorPosition(0)
-        self.position = self.motor.getSelectedSensorPosition()
+        self.encoder = self.motor.getEncoder(counts_per_rev=2480)
+        self.position = self.encoder.getPosition() # This is in rotations
         self.disabled = False
         self.name = name
+        if self.name == "leftShoulder":
+            self.direction = -1
+        else:
+            self.direction = 1
     
-    # WHERE I LAST LEFT OFF --------------------------------------------------------
+    def forward(self, speed):
+        self.motor.set(self.direction*speed)
+        self.disabled = False
+    
+    def backward(self, speed):
+        self.checkEffort()
+        if self.disabled == False:
+            self.motor.set(-1*self.direction*speed)
+            wpilib.SmartDashboard.putBool(self.name + " stopped", False)
+        else:
+            self.motor.set(0)
+            self.motor.setIdleMode(rev.IdleMode.kBrake)
+            wpilib.SmartDashboard.putBool(self.name + " stopped" + True)
+    
+    def checkEffort(self):
+        current = self.motor.getOutputCurrent()
+        velocity = self.encoder.getVelocity()
+        wpilib.SmartDashboard.putNumber("Shoulder Encoder RPM", velocity)
+        if velocity < 2 and current > self.currentLimit:
+            self.disabled = True
+    
+
 
 
 class Winch:
@@ -52,15 +75,19 @@ class Winch:
         self.position = self.motor.getSelectedSensorPosition()
         self.disabled = False
         self.name = name
+        if self.name == "leftWinch":
+            self.direction = -1
+        else:
+            self.direction = 1
 
     def release(self, speed):
-        self.motor.set(ctre.ControlMode.PercentOutput, speed)
+        self.motor.set(ctre.ControlMode.PercentOutput, self.direction*speed)
         self.disabled = False
 
     def retract(self, speed):
         self.checkEffort()
         if self.disabled == False:
-            self.motor.set(ctre.ControlMode.PercentOutput, -speed)
+            self.motor.set(ctre.ControlMode.PercentOutput, -1*self.direction*speed)
             wpilib.SmartDashboard.putBool(self.name + " stopped", False)
         else:
             self.motor.set(ctre.ControlMode.PercentOutput, 0)
@@ -73,7 +100,7 @@ class Winch:
         current = self.motor.getStatorCurrent()
         velocity = self.motor.getSelectedSensorVelocity()
         wpilib.SmartDashboard.putNumber("Winch Sensor Velocity", velocity)
-        if velocity < 0.1 and current > self.currentLimit:
+        if velocity < 1 and current > self.currentLimit:
             self.disabled = True
     
     def getPosition(self):
